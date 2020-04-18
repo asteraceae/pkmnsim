0a016566890da03486cb3f2f1609d20a8ecb4cea
daisenia <58607819+daisenia@users.noreply.github.com>
2020-04-18 18:44:54 -0500

    Add files via upload

--- gui.java
+++ gui.java
@@ -7,15 +7,17 @@ import java.io.File;
 public class gui implements ActionListener {
 
 //basic gui:
-    private final JLabel label; //editable label
     private final JFrame frame; //frame for gui
     private JPanel panel;       //panel to put stuff in
-    private final JLabel image; //image for start
     private JButton b;          //button (Editable)
+
+//add ins that interact w multiple screens
     private JComboBox<String> list; //player 1 drop down menu
+    private JComboBox<String> list2;
 
 //imports gameplay data n logic
-    private final data data = new data();
+    private final data p1 = new data();
+    private final data p2 = new data();
 
 //enables button function to change thru actionperformed
     private boolean x1 = false;
@@ -24,7 +26,7 @@ public class gui implements ActionListener {
 //some colors to use (RGB) + font
     private final Font arial = new Font("Arial", Font.PLAIN, 14);
     private final Font arialb = new Font("Arial", Font.BOLD, 14);
-    private final Color lilac = new Color(227,200,246);
+    private final Color lilac = new Color(190,200,246);
     private final Color pink = new Color(250,208,252);
     private final Color blue = new Color(172,215,247);
 
@@ -32,7 +34,7 @@ public class gui implements ActionListener {
     public gui(){
         //set up frame n label
         frame = new JFrame();
-        label = new JLabel("click to start");
+        JLabel label = new JLabel("click to start");
         label.setHorizontalAlignment(JLabel.CENTER);
         label.setFont(arial);
 
@@ -49,7 +51,7 @@ public class gui implements ActionListener {
         panel.setLayout(new GridLayout(3, 1));
 
         //image settings
-        image = new JLabel();
+        JLabel image = new JLabel();
         image.setIcon(new ImageIcon("start.png"));
 
         //add button, image, label to panel
@@ -77,36 +79,107 @@ public class gui implements ActionListener {
       //enables change to battle screen and exports chosen pkmn
         else if (!x2){
           String selected = (String) list.getSelectedItem();
-          data.setP1(selected);
+          String selected2 = (String) list2.getSelectedItem();
+          p1.setP1(selected);
+          p2.setP1(selected2);
           x3();
           x2 = true;
         }
     }
     public void x2(){
-        panel.remove(b);
-        panel.remove(image);
 
-        list = new JComboBox<>(data.pkmn);
 
-        b = new JButton("Finish");
+        panel.removeAll();
+
+        list = new JComboBox<>(p1.pkmn);
+        list2 = new JComboBox<>(p2.pkmn);
+
+        b = new JButton("start battle");
         b.setBackground(pink);
         b.setForeground(Color.WHITE);
         b.addActionListener(this);
-        label.setText("this would be a choose ur pokemon screen");
-        label.setFont(arial);
         b.setFont(arialb);
+        list.setFont(arial);
+        list2.setFont(arial);
 
-        panel.setLayout(new GridLayout(3, 4));
+        panel.setLayout(new GridLayout(4, 4));
 
-        panel.add(list);
-        panel.add(b, BorderLayout.CENTER);
-        panel.add(label);
+        //t
+        JLabel a = new JLabel();
+        JLabel e = new JLabel();
+        JLabel c = new JLabel();
+        JLabel d = new JLabel();
+        JLabel f = new JLabel();
+
+        JButton r = new JButton(new AbstractAction("randomize!"){
+          public void actionPerformed(ActionEvent e) {
+            p1.randomize();
+            list = new JComboBox<>(p1.pkmn);
+            panel.removeAll();
+            panel.add(p1txt); //c1
+            panel.add(d);
+            panel.add(p2txt);
+            panel.add(list); //c2
+            panel.add(a);
+            panel.add(list2);
+            panel.add(r); //c3
+            panel.add(f);
+            panel.add(r2);
+            panel.add(c);
+            panel.add(b);
+            frame.pack();
+          }
+        });
+        r.setBackground(lilac);
+        r.setForeground(Color.WHITE);
+        r.setFont(arialb);
+
+        JButton r2 = new JButton(new AbstractAction("randomize!"){
+          public void actionPerformed(ActionEvent e) {
+            p2.randomize();
+            list2 = new JComboBox<>(p2.pkmn);
+          }
+        });
+        r2.setBackground(lilac);
+        r2.setForeground(Color.WHITE);
+        r2.setFont(arialb);
+
+        JLabel p1txt = new JLabel();
+        JLabel p2txt = new JLabel();
+        p1txt.setText("Player One");
+        p1txt.setFont(arial);
+        p2txt.setText("Player Two");
+        p2txt.setFont(arial);
+
+        panel.add(p1txt); //c1
+        panel.add(d);
+        panel.add(p2txt);
+        panel.add(list); //c2
+        panel.add(a);
+        panel.add(list2);
+        panel.add(r); //c3
+        panel.add(f);
+        panel.add(r2);
+        panel.add(c);
+        panel.add(b);
+
+        frame.add(panel);
         frame.pack();
     }
     public void x3(){
-        panel.remove(b);
-        panel.remove(list);
+
+        panel.removeAll();
+
+        JLabel label = new JLabel();
+
         label.setText("battle screen :)");
         label.setFont(arial);
+
+        panel.add(label);
+
+
+        frame.add(panel, BorderLayout.CENTER);
+        panel.setBackground(blue);
+        frame.pack();
     }
 }
