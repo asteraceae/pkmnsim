df71a30d6a0b2605a7b8938c7b022006d1475933
daisenia <58607819+daisenia@users.noreply.github.com>
2020-04-27 02:13:20 -0500

    fixed

--- src/pokemon.java
+++ src/pokemon.java
@@ -1,13 +1,13 @@
 public class pokemon {
-  private final int spd;
-  private final int atk;
-  private final int sp;
-  private final int def;;
-  private final int hp;
-  private final String type;
-  private final String type2;
-  private final String name;
-  private final String[] move;
+  public final int spd;
+  public final int atk;
+  public final int sp;
+  public final int def;;
+  public final int hp;
+  public final String type;
+  public final String type2;
+  public final String name;
+  public final String[] move;
 
   public pokemon(int hp, int atk, int def, int spd, int sp, String type, String type2, String name, String[] moves){
     this.spd = spd;
